3050290e2c2da083afce4d7b690850796ef6b339
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-09-02 23:04:21 +0200

    refactor react register component

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import { Formik, Form } from 'formik'
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
-import Wrapper from '../components/Wrapper';
+import React from "react";
+import {Form, Formik} from "formik";
+import {Box, Button,} from "@chakra-ui/react";
+import Wrapper from "../components/Wrapper";
+import {InputField} from "../components/InputField";
 
 interface registerProps {
 }
 
-const Register: React.FC<registerProps> = ({ }) => {
+const Register: React.FC<registerProps> = ({}) => {
     return (
         <Wrapper variant="small">
-        <Formik
-            initialValues={{ username: "", password: "" }}
-            onSubmit={(values) => {
-                console.log(values)
-            }}>
-            {({ values, handleChange }) => (
-                <Form>
-                    <FormControl>
-                        <FormLabel htmlFor="username">Username</FormLabel>
-                        <Input value={values.username} onChange={handleChange} id="username" placeholder="username" />
-                        {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-                    </FormControl>
-                </Form>
-            )}
-        </Formik>
+            <Formik
+                initialValues={{username: "", password: ""}}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {({isSubmitting}) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            colorScheme="teal"
+                            isLoading={isSubmitting}
+                        >
+                            register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
         </Wrapper>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
