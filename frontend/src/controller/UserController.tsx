ae7268a4c1324b5bc4b93a72dff2bd818aae1da3
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-09-03 19:17:35 +0200

    update react frontend

---
+++ frontend/src/controller/UserController.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+import React from "react";
+
+interface UserControllerProps {}
+
+export const UserController: React.FC<> = ({}) => {
+  function registerUser() {
+    console.log("i called register user");
+  }
+};
